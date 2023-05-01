627045a5fabf295a9398e4b3d212eb8df8fdb791
JulienAganze <aganzejulien2017@gmail.com>
2023-05-01 16:17:44 +0200

    quiz solution added

---
+++ Scrapy/tutorial/tutorial/spiders/Quiz1.py
@@ -0,0 +1,26 @@
+# %%
+import scrapy
+
+class ScratchQuotes(scrapy.Spider):
+    name = 'Quiz1'
+    start_urls = ['https://quotes.toscrape.com/page/1/']
+
+
+    def parse(self, response):
+
+        for div in response.css('.quote'):
+            quote = div.css('.text::text').get()
+            author = div.css('.author::text').get()
+            tags = div.css('.tags a::text').getall()
+            tags = ' , '.join(tags)
+            yield {
+                'quote':quote.replace('"','').replace('"','').replace(',',''),
+                'author':author,
+                'tags':tags
+            }
+        
+        nextpageurl = response.css('li.next a::attr(href)').get()
+        print(nextpageurl)
+        if nextpageurl:
+            yield response.follow(nextpageurl, callback=self.parse)
+        
